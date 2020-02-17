e4c0d0db52296cfe26a42d598bd6a1d4fe723e40
bishal giri <giribishal09@gmail.com>
2020-02-17 15:35:47 +0545

    Updates

---
+++ precache-manifest.fb32d130b757aba02abdf0a6a7685093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e916b0ac1424259e74ef199953ce678",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "33308dbd6b61e1d64d3b",
+    "url": "/rolodex/static/css/main.c155ff43.chunk.css"
+  },
+  {
+    "revision": "578b5983e31339b26b19",
+    "url": "/rolodex/static/js/2.02d23424.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/rolodex/static/js/2.02d23424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33308dbd6b61e1d64d3b",
+    "url": "/rolodex/static/js/main.ac5187fd.chunk.js"
+  },
+  {
+    "revision": "5c6c423e43f032b7fc55",
+    "url": "/rolodex/static/js/runtime-main.b46d629a.js"
+  }
+]);
